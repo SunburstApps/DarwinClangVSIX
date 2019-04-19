2d626ea3f0d9be3d2e8edd28f369cfeb63b0a904
William Kent <wjk@users.noreply.github.com>
2019-04-19 15:30:29 -0400

    Add project files.

---
+++ src/Sunburst.VisualStudio.DarwinClangSupport/DarwinClangSupportPackage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace Sunburst.VisualStudio.DarwinClangSupport
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0")]
+    [Guid(PackageGuidString)]
+    public sealed class DarwinClangSupportPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "f1997ed1-4870-4a93-b6b4-64fd964050f4";
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Sunburst.VisualStudio.DarwinClangSupport/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Visual C++ Darwin Support")]
+[assembly: AssemblyCompany("William Kent")]
+[assembly: AssemblyProduct("Visual C++ Darwin Support")]
+[assembly: AssemblyCopyright("Copyright © 2019 William Kent. All rights reserved.")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
